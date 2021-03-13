146251f57452f5776f98d621f349a25888846ac8
atoullo <atoulloraimov@gmail.com>
2021-03-13 21:47:56 +0500

    initial app2

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const keys = require('../config/keys')
 const router = express.Router()
-const keys = require('../config/keys')
 
 const stripe = require('stripe')(keys.stripePublishableKey)
 
